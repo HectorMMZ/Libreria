e5ead7a791c398779d27eaf49e74453b862d959e
Eduard Alberto Camara Quej <eduardcamara002@gmail.com>
2026-05-12 21:14:53 -0600

    Cambio en la barra de tareas, del catalogo solo esta terminado el html y el css falta el js, el carrito solo esta terminado el html y el css falta el js

---
+++ scriptCatalogo.js
@@ -0,0 +1,15 @@
+const LibrosArray = [
+    // objeto
+    {
+        id: "libro-01", //Su id del libro
+        titulo:"Titulo 01", //Este es el nombre que aparecera la pagina
+        imagen: "", //Aqui la imagen
+        categoria:{
+            nombre:  "LibroRomance",  //Esto es necesario para que se pueda separar por generos
+            id:"Romance"
+        },
+        precio: 100, //El precio
+
+    }
+
+]
